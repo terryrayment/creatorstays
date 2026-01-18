7bffd3d120de913b377ea5650c68d72fad98a971
Terry Rayment <terry@friendsandfamily.tv>
2026-01-18 13:39:08 -0800

    j

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -13,7 +13,7 @@ function getStripeClient(): Stripe {
       throw new Error('STRIPE_SECRET_KEY is not set');
     }
     _stripe = new Stripe(secretKey, {
-      apiVersion: '2024-12-18.acacia',
+      apiVersion: '2025-02-24.acacia',
       typescript: true,
     });
   }
